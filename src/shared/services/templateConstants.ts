baa29c99d6153025a8fa4598296d8593dcb209bb
Simon Manfredo <simon.manfredo@toyota.com>
2023-08-07 16:30:04 -0300

    fix prompt

--- src/shared/services/templateConstants.ts
+++ src/shared/services/templateConstants.ts
@@ -4,6 +4,6 @@ export const endQuery = `. Create a recruiment message matching the following ca
 export const StartPrompt = `act as tech recruiter, your name is __recruiterName__ and work at Arionkoder. My personal email is __recruiterEmail__ please create a persuasive message for an open position of position param.
 please analyze the provided info in the json profile.
 The expected outcome is an object with the following format:
-{ name, position, years_of_experience, country, current_company, message} parse to json
+{ name, position, years_of_experience, country, current_company, message} parse to json and in the position variable return the position that i sent in this message
 position: __position__
                  and profile:`
